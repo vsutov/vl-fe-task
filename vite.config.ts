a141312882aab082eacdd64394cfc59bd9c7b928
Vladislav Šutov <vladislav.sutov@bigbank.ee>
2023-05-05 20:18:13 +0300

    vl-fe-task - mkcert for https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  server: {
+    https: true,
+  },
+  plugins: [svelte(), mkcert()],
 })
